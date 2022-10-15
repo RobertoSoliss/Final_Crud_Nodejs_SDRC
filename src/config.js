9fc1032fa507198715d05387d3e38d6ea0a157ed
RobertoSoliss <championssolis@gmail.com>
2022-10-15 15:59:05 -0500

    subida

--- src/config.js
+++ src/config.js
@@ -7,4 +7,6 @@ export const DB_port = process.env.DB_port || '3306'
 
 
 
-export const dbconfig = process.env.MYSQL_URL || "mysql://root:celisSOLIS1027@localhost:3306/prueba"
+//export const dbconfig = process.env.MYSQL_URL || "mysql://root:celisSOLIS1027@localhost:3306/prueba"
+export const dbconfig = process.env.MYSQL_URL || "mysql://${{ MYSQLUSER }}:${{ MYSQLPASSWORD }}@${{ MYSQLHOST }}:${{ MYSQLPORT }}/${{ MYSQLDATABASE }}"
+
--- src/mysqldb.js
+++ src/mysqldb.js
@@ -8,3 +8,29 @@ const dbcon = parseDatabaseUrl(dbconfig)
 export const db = createConnection(dbcon);
 
 db.connect()
+
+
+//----------------------
+/*
+const mysql = require('mysql2')
+export const conexion = mysql.createConnection({
+
+    host: 'localhost',
+    user: 'root',
+    password: 'celisSOLIS1027',
+    database: 'prueba',
+
+
+})
+
+conexion.connect((error)=>{
+
+    if(error){
+        console.log("Error al conectar la BD")
+        return
+    }else{
+        console.log("Conexión exitosa con la BD")
+    }
+
+});
+*/
