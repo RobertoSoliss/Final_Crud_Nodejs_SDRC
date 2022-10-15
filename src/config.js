8c4a2d8ffbe1d3baf18b3b106bee4556b0a3286d
RobertoSoliss <championssolis@gmail.com>
2022-10-15 04:09:12 -0500

    subida2

--- src/config.js
+++ src/config.js
@@ -1,3 +1,10 @@
 export const port = process.env.port || 3000
+export const DB_HOST = process.env.DB_HOST || 'localhost'
+export const DB_USER = process.env.DB_USER || 'root'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'celisSOLIS1027'
+export const DB_NAME = process.env.DB_NAME || 'prueba'
+export const DB_port = process.env.DB_port || '3306'
+
+
 
 export const dbconfig = process.env.MYSQL_URL || "mysql://root:celisSOLIS1027@localhost:3306/prueba"
